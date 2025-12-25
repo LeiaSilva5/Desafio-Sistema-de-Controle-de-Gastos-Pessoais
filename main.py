2d8bcb04ef3a8786bf70febed6650a14716478db
LeiaSilva5 <leiaferreira457@gmail.com>
2025-12-25 16:09:31 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,38 @@
+
+# opções do menu
+def op_menu(opcao):
+    if int(opcao) == 1:
+        print('Adicionar Gastos')
+    elif int(opcao) == 2:
+        print('Listar gastos')
+    elif int(opcao) == 3:
+        print('Listar gastos')
+    elif int(opcao) == 4:
+        print('Listar gastos')
+    elif int(opcao) == 5:
+        print('Listar gastos')
+    else:
+        print('Opção invalida, deseja retornar par o menu inicial ? [S] [N]')
+        item = input(": ")
+        if item.upper() == 'S':
+            menu()
+        elif item.upper() == 'N':
+            print("Até mais!")    
+
+# Menu principal
+def menu():
+    validador = True
+    while validador:
+        print("----- Sistema de Controle de Gastos Pessoais -----")
+        print("Digite uma opção abaixo: ")
+        print("1 - Adicionar gasto \n2 - Listar gastos \n3 - Mostrar total gasto \n4 - Mostrar gasto médio \n5 - Sair")
+        op = input("Digite aqui: ") 
+        if int(op) == 5:
+            print("Até mais !")
+            validador = False   
+        else:
+            op_menu(op)
+        
+            
+menu()
+    
